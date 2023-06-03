b38960936d309c6bc8f95d75b2cadef69d47bda4
vaismand <vaisman.david91@gmail.com>
2023-06-03 14:50:15 +0200

    Extra check for rectangular

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: dvaisman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 22:13:28 by dvaisman          #+#    #+#             */
-/*   Updated: 2023/06/03 14:43:22 by dvaisman         ###   ########.fr       */
+/*   Updated: 2023/06/03 14:50:05 by dvaisman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ void	check_rows(t_game *game)
 	if (game->map.full[i][0] != '1' ||
 			game->map.full[i][game->map.columns - 1] != '1')
 			ft_error_msg("Map is not closed", game);
+	if (game->map.columns != ft_strlen(game->map.full[i]))
+			ft_error_msg("Map is not rectangular", game);
 }
 
 void	count_map_param(t_game *game)
